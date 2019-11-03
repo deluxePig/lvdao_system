dbea62b9a749815fc1526ccfa7ff0798b84f0ff0
马晋威 <mjw@majinweideMacBook-Pro.local>
2019-11-03 21:42:32 +0800

    设备管理静态页

--- greenRoad/src/views/deviceManage/deviceManage.vue
+++ greenRoad/src/views/deviceManage/deviceManage.vue
@@ -1,12 +1,288 @@
 <!-- 设备管理-->
 <template>
-  <div id="deviceManage">
-   设备管理
-  </div>
+    <div id="deviceManage">
+        <div class="deviceManage-box">
+            <p class="deviceManage-title">设备管理</p>
+            <div class="deviceManage-header">
+                <el-row>
+                    <el-col :span="2"><div>站点名称</div></el-col>
+                    <el-col :span="3"><div>站点编号</div></el-col>
+                    <el-col :span="2"><div>蓝牙信标数量</div></el-col>
+                    <el-col :span="3"><div>MAC地址</div></el-col>
+                    <el-col :span="2"><div>信号强度</div></el-col>
+                    <el-col :span="2"><div>电池电量</div></el-col>
+                    <el-col :span="3"><div>上线时间</div></el-col>
+                    <el-col :span="3"><div>下线时间</div></el-col>
+                    <el-col :span="2"><div>下线原因</div></el-col>
+                    <el-col :span="2"><div>历史故障</div></el-col>
+                </el-row>
+            </div>
+            <div class="deviceManage-items">
+                <el-row class="device-content">
+                    <el-col :span="2">
+                        <div>柳湖花园</div>
+                    </el-col>
+                    <el-col :span="3">
+                        <div>057901001</div>
+                    </el-col>
+                    <el-col :span="2">
+                        <div>8</div>
+                    </el-col>
+                    <el-col :span="3">
+                        <div>
+                            <p>E3:56:G1:76:61</p>
+                            <p>E3:56:G1:76:62</p>
+                            <p>E3:56:G1:76:63</p>
+                            <p>E3:56:G1:76:64</p>
+                            <p>E3:56:G1:76:65</p>
+                        </div>
+                    </el-col>
+                    <el-col :span="2">
+                        <div>
+                            <p>80</p>
+                            <p>80</p>
+                            <p>80</p>
+                            <p>80</p>
+                            <p>80</p>
+                        </div>
+                    </el-col>
+                    <el-col :span="2">
+                        <div>
+                            <p>90%</p>
+                            <p>90%</p>
+                            <p>90%</p>
+                            <p>90%</p>
+                            <p>90%</p>
+                        </div>
+                    </el-col>
+                    <el-col :span="3">
+                        <div>
+                            <p>2019.09.11</p>
+                            <p>2019.09.11</p>
+                            <p>2019.09.11</p>
+                            <p>2019.09.11</p>
+                            <p>2019.09.11</p>
+                        </div>
+                    </el-col>
+                    <el-col :span="3">
+                        <div>2019.0912</div>
+                    </el-col>
+                    <el-col :span="2">
+                        <div>遗失</div>
+                    </el-col>
+                    <el-col :span="2">
+                        <div>
+                            <p>查看</p>
+                            <p>查看</p>
+                            <p>查看</p>
+                            <p>查看</p>
+                            <p>查看</p>
+                        </div>
+                    </el-col>
+                </el-row>
+            </div>
+
+            <div class="deviceManage-items">
+                <el-row class="device-content">
+                    <el-col :span="2">
+                        <div>柳湖花园</div>
+                    </el-col>
+                    <el-col :span="3">
+                        <div>057901001</div>
+                    </el-col>
+                    <el-col :span="2">
+                        <div>8</div>
+                    </el-col>
+                    <el-col :span="3">
+                        <div>
+                            <p>E3:56:G1:76:61</p>
+                            <p>E3:56:G1:76:62</p>
+                            <p>E3:56:G1:76:63</p>
+                            <p>E3:56:G1:76:64</p>
+                            <p>E3:56:G1:76:65</p>
+                        </div>
+                    </el-col>
+                    <el-col :span="2">
+                        <div>
+                            <p>80</p>
+                            <p>80</p>
+                            <p>80</p>
+                            <p>80</p>
+                            <p>80</p>
+                        </div>
+                    </el-col>
+                    <el-col :span="2">
+                        <div>
+                            <p>90%</p>
+                            <p>90%</p>
+                            <p>90%</p>
+                            <p>90%</p>
+                            <p>90%</p>
+                        </div>
+                    </el-col>
+                    <el-col :span="3">
+                        <div>
+                            <p>2019.09.11</p>
+                            <p>2019.09.11</p>
+                            <p>2019.09.11</p>
+                            <p>2019.09.11</p>
+                            <p>2019.09.11</p>
+                        </div>
+                    </el-col>
+                    <el-col :span="3">
+                        <div>2019.0912</div>
+                    </el-col>
+                    <el-col :span="2">
+                        <div>遗失</div>
+                    </el-col>
+                    <el-col :span="2">
+                        <div>
+                            <p>查看</p>
+                            <p>查看</p>
+                            <p>查看</p>
+                            <p>查看</p>
+                            <p>查看</p>
+                        </div>
+                    </el-col>
+                </el-row>
+            </div>
+
+            <div class="deviceManage-items">
+            <el-row class="device-content">
+                <el-col :span="2">
+                    <div>柳湖花园</div>
+                </el-col>
+                <el-col :span="3">
+                    <div>057901001</div>
+                </el-col>
+                <el-col :span="2">
+                    <div>8</div>
+                </el-col>
+                <el-col :span="3">
+                    <div>
+                        <p>E3:56:G1:76:61</p>
+                        <p>E3:56:G1:76:62</p>
+                        <p>E3:56:G1:76:63</p>
+                        <p>E3:56:G1:76:64</p>
+                        <p>E3:56:G1:76:65</p>
+                    </div>
+                </el-col>
+                <el-col :span="2">
+                    <div>
+                        <p>80</p>
+                        <p>80</p>
+                        <p>80</p>
+                        <p>80</p>
+                        <p>80</p>
+                    </div>
+                </el-col>
+                <el-col :span="2">
+                    <div>
+                        <p>90%</p>
+                        <p>90%</p>
+                        <p>90%</p>
+                        <p>90%</p>
+                        <p>90%</p>
+                    </div>
+                </el-col>
+                <el-col :span="3">
+                    <div>
+                        <p>2019.09.11</p>
+                        <p>2019.09.11</p>
+                        <p>2019.09.11</p>
+                        <p>2019.09.11</p>
+                        <p>2019.09.11</p>
+                    </div>
+                </el-col>
+                <el-col :span="3">
+                    <div>2019.0912</div>
+                </el-col>
+                <el-col :span="2">
+                    <div>遗失</div>
+                </el-col>
+                <el-col :span="2">
+                    <div>
+                        <p>查看</p>
+                        <p>查看</p>
+                        <p>查看</p>
+                        <p>查看</p>
+                        <p>查看</p>
+                    </div>
+                </el-col>
+            </el-row>
+        </div>
+
+            <div class="deviceManage-items">
+                <el-row class="device-content">
+                    <el-col :span="2">
+                        <div>柳湖花园</div>
+                    </el-col>
+                    <el-col :span="3">
+                        <div>057901001</div>
+                    </el-col>
+                    <el-col :span="2">
+                        <div>8</div>
+                    </el-col>
+                    <el-col :span="3">
+                        <div>
+                            <p>E3:56:G1:76:61</p>
+                            <p>E3:56:G1:76:62</p>
+                            <p>E3:56:G1:76:63</p>
+                            <p>E3:56:G1:76:64</p>
+                            <p>E3:56:G1:76:65</p>
+                        </div>
+                    </el-col>
+                    <el-col :span="2">
+                        <div>
+                            <p>80</p>
+                            <p>80</p>
+                            <p>80</p>
+                            <p>80</p>
+                            <p>80</p>
+                        </div>
+                    </el-col>
+                    <el-col :span="2">
+                        <div>
+                            <p>90%</p>
+                            <p>90%</p>
+                            <p>90%</p>
+                            <p>90%</p>
+                            <p>90%</p>
+                        </div>
+                    </el-col>
+                    <el-col :span="3">
+                        <div>
+                            <p>2019.09.11</p>
+                            <p>2019.09.11</p>
+                            <p>2019.09.11</p>
+                            <p>2019.09.11</p>
+                            <p>2019.09.11</p>
+                        </div>
+                    </el-col>
+                    <el-col :span="3">
+                        <div>2019.0912</div>
+                    </el-col>
+                    <el-col :span="2">
+                        <div>遗失</div>
+                    </el-col>
+                    <el-col :span="2">
+                        <div>
+                            <p>查看</p>
+                            <p>查看</p>
+                            <p>查看</p>
+                            <p>查看</p>
+                            <p>查看</p>
+                        </div>
+                    </el-col>
+                </el-row>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
     import deviceManage from './deviceManage'
+
     export default deviceManage
 </script>
 <style scoped src="./deviceManage.less?time=New Date()">
