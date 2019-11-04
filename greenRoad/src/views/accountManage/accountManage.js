f48d008ce97ff37b53e5f6ccff72c3f1b8efa3c5
马晋威 <mjw@majinweideMacBook-Pro.local>
2019-11-04 17:08:35 +0800

    导航栏编写

---
+++ greenRoad/src/views/accountManage/accountManage.js
@@ -0,0 +1,23 @@
+
+export default {
+    name: 'accountManage',
+    components: {
+    },
+    data (){
+        return{
+
+        }
+    },
+    created:function(){
+
+    },
+    mounted(){
+
+    },
+    methods:{
+
+    },
+    watch:{
+
+    }
+}
